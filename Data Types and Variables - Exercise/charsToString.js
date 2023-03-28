028ba7799f77b1499e15aee227a29597f17f81c4
Petar Cholakov <petar.cholakov30@gmail.com>
2023-03-28 13:07:17 +0100

    Create charsToString.js

---
+++ Data Types and Variables - Exercise/charsToString.js
@@ -0,0 +1,10 @@
+function charsToStrings(charOne, charTwo, charThree) {
+
+    let result = '';
+    result += charOne + charTwo + charThree;
+    console.log(result);
+}
+
+charsToStrings(1,
+    5,
+    'p')
