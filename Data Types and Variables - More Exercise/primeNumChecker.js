38c17b275b4afb210c92d5a98818ea5be0601e71
Petar Cholakov <petar.cholakov30@gmail.com>
2023-04-23 20:21:36 +0100

    Update primeNumChecker.js

--- Data Types and Variables - More Exercise/primeNumChecker.js
+++ Data Types and Variables - More Exercise/primeNumChecker.js
@@ -1,3 +1,12 @@
+// Write a function to check if a number is prime (only divisible by itself and one).
+// The input comes as a single number argument.
+// The output should be the return value of your function. Return true for prime number and false otherwise.
+// Examples
+// Input	Output
+// 7	true
+// 8	false
+// 81	false
+
 function primeNumChecker(num) {
 
     let primeCounter = 0;
