1983b8c43084adb290e64615999a5a7a4756ef44
Petar Cholakov <petar.cholakov30@gmail.com>
2023-05-14 07:37:18 +0100

    Create loadingBar.js

---
+++ Functions - Exercise/loadingBar.js
@@ -0,0 +1,26 @@
+function loadingBar(number) {
+
+    let result = ['.', '.', '.', '.','.','.','.','.','.','.'];
+    
+    for (let i = 10; i <= number; i += 10) {
+        result.unshift('%');
+        result.pop();
+    }
+
+    result.push(']');
+    result.unshift('[')
+    result.unshift(`${number}% `)
+    
+
+    if (number < 100) {
+        console.log(result.join(''));
+        console.log('Still loading...');
+    } else {
+        console.log('100% Complete!');
+        console.log('[%%%%%%%%%%]')
+    }
+}
+
+loadingBar(30);
+loadingBar(50);
+loadingBar(100);
