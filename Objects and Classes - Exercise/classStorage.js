0d0e5896afb32d7d129fd0424b06835c7a43c52d
Petar Cholakov <petar.cholakov30@gmail.com>
2023-06-24 21:21:22 +0100

    Create classStorage.js

---
+++ Objects and Classes - Exercise/classStorage.js
@@ -0,0 +1,23 @@
+class Storage {
+    constructor(capacity) {
+        this.capacity = capacity;
+    }
+    
+    addProduct(input) {
+        if (storage.capacity - input.quantity > 0) {
+            storage['counter'] = input;
+            storage.capacity -= input.quantity;    
+        }
+    }
+}
+
+
+let productOne = {name: 'Cucamber', price: 1.50, quantity: 15};
+let productTwo = {name: 'Tomato', price: 0.90, quantity: 25};
+let productThree = {name: 'Bread', price: 1.10, quantity: 8};
+
+let storage = new Storage(50);  
+
+storage.addProduct(productOne);
+storage.addProduct(productTwo);
+storage.addProduct(productThree);
