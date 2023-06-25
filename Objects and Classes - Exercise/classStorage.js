cd2a4493889e8dc4ed432918ac3bdf68b302970d
Petar Cholakov <petar.cholakov30@gmail.com>
2023-06-25 20:56:48 +0100

    Update classStorage.js

--- Objects and Classes - Exercise/classStorage.js
+++ Objects and Classes - Exercise/classStorage.js
@@ -1,23 +1,46 @@
 class Storage {
     constructor(capacity) {
         this.capacity = capacity;
+        this.storage = [];
     }
-    
+
+    get totalCost() {        
+        return this.storage.reduce((acc, el) => {return acc + el.price * el.quantity}, 0);
+    }
+
     addProduct(input) {
-        if (storage.capacity - input.quantity > 0) {
-            storage['counter'] = input;
-            storage.capacity -= input.quantity;    
-        }
+        this.storage.push(input);
+        this.capacity -= input.quantity;
+        return;
+    }
+
+    getProducts() {
+        let finalResult = [];
+        this.storage.forEach(obj => {
+            finalResult.push(JSON.stringify(obj))
+        })
+        return finalResult.join('\n');
     }
 }
 
+let productOne = { name: 'Cucamber', price: 1.50, quantity: 15 };
+let productTwo = { name: 'Tomato', price: 0.90, quantity: 25 };
+let productThree = { name: 'Bread', price: 1.10, quantity: 8 };
 
-let productOne = {name: 'Cucamber', price: 1.50, quantity: 15};
-let productTwo = {name: 'Tomato', price: 0.90, quantity: 25};
-let productThree = {name: 'Bread', price: 1.10, quantity: 8};
-
-let storage = new Storage(50);  
+let storage = new Storage(50);
 
 storage.addProduct(productOne);
 storage.addProduct(productTwo);
 storage.addProduct(productThree);
+
+console.log(storage.getProducts());
+console.log(storage.capacity);
+console.log(storage.totalCost);
+
+
+// let productOne = {name: 'Tomato', price: 0.90, quantity: 19};
+// let productTwo = {name: 'Potato', price: 1.10, quantity: 10};
+// let storage = new Storage(30);
+// storage.addProduct(productOne);
+// storage.addProduct(productTwo);
+// console.log(storage.totalCost);
