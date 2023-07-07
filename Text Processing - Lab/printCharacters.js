3a5ec4276f252bd9065d80392787e75a2ad2764b
Petar Cholakov <petar.cholakov30@gmail.com>
2023-07-07 14:33:46 +0100

    Create printCharacters.js

---
+++ Text Processing - Lab/printCharacters.js
@@ -0,0 +1,9 @@
+function printCharacters(input) {
+
+    for (let i = 0; i < input.length; i++) {
+        console.log(input[i]);
+    }
+}
+
+// printCharacters('AWord');
+printCharacters('Sentence');
