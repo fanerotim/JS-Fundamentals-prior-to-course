da15966f980723f2a4c5bc3bf0b929d8ed5512f9
Petar Cholakov <petar.cholakov30@gmail.com>
2023-07-07 14:33:56 +0100

    Create substring.js

---
+++ Text Processing - Lab/substring.js
@@ -0,0 +1,8 @@
+function substring(input, startIndex, count) {
+
+    let result = input.substring(startIndex, count + startIndex);
+    console.log(result);
+}
+
+substring('ASentence', 1, 8);
+substring('SkipWord', 4, 7)
