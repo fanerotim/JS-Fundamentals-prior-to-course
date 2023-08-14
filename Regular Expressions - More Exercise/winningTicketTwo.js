a1327475e518b331a260e800c76de056ffc5d840
Petar Cholakov <petar.cholakov30@gmail.com>
2023-08-14 17:20:30 +0100

    Create winningTicketTwo.js

---
+++ Regular Expressions - More Exercise/winningTicketTwo.js
@@ -0,0 +1,49 @@
+function winningTicket(input) {
+
+    let text = input;
+    // let pattern = /(.[^@#$\^,\s]){0,4}(?<left>[@]{6,10}|[#]{6,10}|[$]{6,10}|[\^]{6,10}])(.[^@#$\^,\s]){0,4}(?<right>\2)(.[^@#$\^,\s]){0,4}/g;
+    let pattern = /(\@{6,10}|\${6,10}|\^{6,10}|\#{6,10})/g;
+    let ticketsArr = text.trim().split(/\s*,\s*/);
+
+    for (let item of ticketsArr) {
+
+        let match = pattern.exec(item);
+
+        if (item.length !== 20) {
+            console.log("invalid ticket");
+            continue;
+        } else {
+
+            let left = item.toString().substring(0, item.length / 2).match(pattern);
+            let right = item.toString().substring(item.length / 2).match(pattern);
+
+            if (left === null || right === null) {
+                console.log(`ticket "${item}" - no match`);
+                continue;
+            }
+
+            left = left.toString();
+            right = right.toString();
+
+            if (left.length === 10 && right.length === 10) {
+                console.log(`ticket "${item}" - ${left.length}${left[0]} Jackpot!`)
+            } else if (left[0] == right[0]) {
+
+                if (left.length < right.length) {
+                    console.log(`ticket "${item}" - ${left.length}${left[0]}`)
+                } else if (right.length < left.length) {
+                    console.log(`ticket "${item}" - ${right.length}${left[0]}`)
+                } else {
+                    console.log(`ticket "${item}" - ${left.length}${left[0]}`)
+                }
+            } else {
+                console.log(`ticket "${item}" - no match`);
+            }
+        }
+    }
+}
+
+winningTicket('Cash$$$$$$Ca$$$$$$sh');
+winningTicket('$$$$$$$$$$$$$$$$$$$$, aabb , th@@@@@@eemo@@@@@@ey');
+winningTicket('validticketnomatch:(');
+winningTicket('Cash$$$$$$Ca@@@@@@sh');
